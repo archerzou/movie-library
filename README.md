04871fa1949cf63bd8ea901118adc8d5ebb3c4a5
unknown <archerzou>
2020-11-30 01:51:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Movie Application
+
+Movie library built with React and TMDb API.
+
+##  Development
+
+```bash
+$ git clone https://github.com/archerzou/movie-library.git
+$ npm install
+$ npm start
+```
+
+This will serve the files at `http://localhost:3000/`
+
+### Environment Variables
+In order for this app to work, you will need an API key. You can get this from [TMDb Website](https://developers.themoviedb.org/3/getting-started/introduction)
+
+Create a copy of the .env.example file, and change the file name to `.env`
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="footer-content">
+                    <p>© Archer Zou</p>
+                    <a
+                        href="https://github.com/archerzou/movie-library"
+                        target="_blank"
+                    >
+                        Repository
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
